b7654b32da453ff285f152cb02dd3111aa4032d1
Scott Moreau <oreaus@gmail.com>
2026-07-11 09:15:26 -0600

    Fix crash on shutdown after grouping windows

--- wf-plugin/gtkdecor.cpp
+++ wf-plugin/gtkdecor.cpp
@@ -1061,7 +1061,7 @@ static void handle_deco_client_destroy(struct wl_listener*, void*)
 
         for (auto & v : wf::get_core().get_all_views())
         {
-            if (!v || !v->is_mapped() || !wf::toplevel_cast(v))
+            if (v->role != wf::VIEW_ROLE_TOPLEVEL)
             {
                 continue;
             }
@@ -1072,7 +1072,7 @@ static void handle_deco_client_destroy(struct wl_listener*, void*)
 
     for (auto & v : wf::get_core().get_all_views())
     {
-        if (!wf::toplevel_cast(v))
+        if (v->role != wf::VIEW_ROLE_TOPLEVEL)
         {
             continue;
         }
