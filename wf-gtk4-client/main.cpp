73759c40f33e5619eae4a095ace6a81c36d32937
Scott Moreau <oreaus@gmail.com>
2026-07-17 03:51:15 -0600

    Allow multiple instances of wf-gtk4-decorator client
    This is useful if you run multiple compositors.

--- wf-gtk4-client/main.cpp
+++ wf-gtk4-client/main.cpp
@@ -570,7 +570,7 @@ int main(int argc, char **argv)
 {
     int status;
 
-    app = gtk_application_new("org.wf.sample-decorator", G_APPLICATION_DEFAULT_FLAGS);
+    app = gtk_application_new("org.wf.sample-decorator", G_APPLICATION_NON_UNIQUE);
     g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
     status = g_application_run(G_APPLICATION(app), argc, argv);
     g_object_unref(app);
